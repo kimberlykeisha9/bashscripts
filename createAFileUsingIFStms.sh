9b720fb2a2676d2aafdc736aeddcb2123b44b583
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2021-10-11 13:12:38 +0300

    Add a Todo and comments

--- createAFileUsingIFStms.sh
+++ createAFileUsingIFStms.sh
@@ -1,9 +1,14 @@
 #!/bin/bash/
 
-#Prompt the user to enter a file name
-echo "Enter a file name" 
+# Prompt the user to enter a file name
 
+echo "Enter a file name"
+
+# Read the inputted name
 read filename 
+# ----------------------
+#   TODO -> Validate the input
+# ----------------------
 
 echo "Do you want to save $filename?"
 
